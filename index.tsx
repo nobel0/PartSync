14998bb54f20ef58d48fce0728bb09862c1452ce
Abdulhadi Joharji <nobel.ninja@gmail.com>
2026-01-24 19:01:17 +0300

    fixing the ap editing taxonomy etc V49

--- index.tsx
+++ index.tsx
@@ -1,10 +1,18 @@
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { storageService } from './services/storageService';
 
 const rootElement = document.getElementById('root');
 if (!rootElement) {
   throw new Error("Could not find root element to mount to");
+}
+
+// Immediate title update from storage to avoid flicker
+const config = storageService.getConfig();
+if (config.browserTitle) {
+  document.title = config.browserTitle;
 }
 
 const root = ReactDOM.createRoot(rootElement);
